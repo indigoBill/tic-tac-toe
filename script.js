48f1a9a51cc1dd9ee0744f20dc5d8cb1e1e171ac
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-12-13 09:41:15 -0500

    Create a display game object

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const GameBoard = (function(){
     function addMark(mark, boardIndex){
         board.splice(boardIndex, 1, mark);
         console.log(getBoard());
+        
     }
 
     return {getBoard, resetBoard, addMark};
@@ -24,20 +25,24 @@ const Player = function(name = 'Player'){
 
     const board = GameBoard.getBoard();
 
-    const getName = function(){
+    function getName(){
         return name;
     }
 
-    const setMarker = function(playerMarker){
+    function setName(newName){
+        name = newName;
+    }
+
+    function setMarker(playerMarker){
         marker = playerMarker;
     }
 
-    const getMarker = function(){
+    function getMarker(){
         return marker;
     }
 
-    const getPlayerMove = function(){
-        let boardPosition = Number(prompt('YOUR TURN. PICK A POSITION'));
+    function getPlayerMove(){
+        let boardPosition = Number(prompt(`${name}: YOUR TURN. PICK A POSITION`));
 
         if(board[boardPosition] === ''){
             console.log(`SUCCESS: ${boardPosition}`);
@@ -47,7 +52,7 @@ const Player = function(name = 'Player'){
         }
     }
 
-    return {getName, setMarker, getMarker, getPlayerMove};
+    return {getName, setName, getMarker, setMarker, getPlayerMove};
 }
 
 const Computer = function(name = 'Computer'){
@@ -55,19 +60,19 @@ const Computer = function(name = 'Computer'){
 
     const board = GameBoard.getBoard();
 
-    const getName = function(){
+    function getName(){
         return name;
     }
 
-    const setMarker = function(computerMarker){
+    function setMarker(computerMarker){
         marker = computerMarker;
     }
 
-    const getMarker = function(){
+    function getMarker(){
         return marker;
     }
 
-    const getComputerMove = function(){
+    function getComputerMove(){
         let availablePositions = [];
 
         board.forEach((element, index) => {
@@ -81,83 +86,79 @@ const Computer = function(name = 'Computer'){
         return availablePositions[getRandomPositionIndex()];
     }
 
-    return {getName, setMarker, getMarker, getComputerMove};
+    return {getName, getMarker, setMarker, getComputerMove};
 }
 
 const GameFlow = (function(){
-
-    const board = GameBoard.getBoard();
-    let playerMark;
-    let computerMark;
+    let activePlayer;
     let activeMarker;
     let isWinner = false;
-
-    let activePlayer;
-
+    
+    const board = GameBoard.getBoard();
     const player = new Player();
     const computer = new Computer();
 
-    function assignPlayerMarker(){
+    let player1;
+    let player2;
+
+    function chooseOpponent(){
+        const opponent = prompt('TYPE \'C\' TO PLAY AGAINST THE COMPUTER OR \'P\' TO PLAY AGAINST ANOTHER PLAYER').toUpperCase();
+
+        if(opponent === 'C'){
+            player1 = new Player();
+            player2 = new Computer();
+        }else if(opponent === 'P'){
+            player1 = new Player('Player 1');
+            player2 = new Player('Player 2');
+        }else{
+            console.log('ENTER VALID VALUE');
+            chooseOpponent();
+        }   
+    }
+
+    function assignFirstMarker(){
         const mark = prompt('CHOOSE A MARK: X OR O').toUpperCase();
 
         if(mark === 'X' || mark === 'O'){
-            player.setMarker(mark);
-            assignComputerMarker();
+            player1.setMarker(mark);
+            assignSecondMarker();
 
-            console.log(`${player.getName()} : ${player.getMarker()}`);
-            console.log(`${computer.getName()} : ${computer.getMarker()}`);
-          
+            console.log(`${player1.getName()} : ${player1.getMarker()}`);
+            console.log(`${player2.getName()} : ${player2.getMarker()}`);
         }else{
             console.log('PLEASE CHOOSE EITHER X OR O');
-            assignPlayerMarker();
+            assignFirstMarker();
         }
     }
 
-    function assignComputerMarker(){
-        const mark = player.getMarker() === 'X' ? 'O' : 'X';
-        computer.setMarker(mark);
+    function assignSecondMarker(){
+        const mark = player1.getMarker() === 'X' ? 'O' : 'X';
+        player2.setMarker(mark);
     }
 
     function playXFirst(){
-        if(player.getMarker() === 'X'){
-            GameBoard.addMark(player.getMarker(), player.getPlayerMove());
-            activePlayer = player.getName();
+        if(player1.getMarker() === 'X'){
+            GameBoard.addMark(player1.getMarker(), player1.getPlayerMove());
+            activePlayer = player1.getName();
+            activeMarker = player1.getMarker();
         } else{
-            GameBoard.addMark(computer.getMarker(), computer.getComputerMove());
-            activePlayer = computer.getName();
-        }
-    }
-
-    //FUNCTIONS ABOVE ALREADY EDITTED
-    //CHECK FUNCTIONS BELOW
-
-    function checkForEmptySpaces(positionValue){
-        return positionValue !== '';
-    }
-
-    function playGame(){
-
-        while(!(board.every(checkForEmptySpaces)) && isWinner == false){
-            let currentMove;
-            switchPlayer();
-
-            if(activeMarker == playerMark){
-                currentMove = getPlayerPlay();
-                GameBoard.addMark(playerMark, currentMove);
+            if(player2.hasOwnProperty('getComputerMove')){
+                GameBoard.addMark(player2.getMarker(), player2.getComputerMove());
             }else{
-                currentMove = getComputerPlay();
-                GameBoard.addMark(computerMark, currentMove);
+                GameBoard.addMark(player2.getMarker(), player2.getPlayerMove());
             }
-            checkForWinner(currentMove);
-        }
-        
-        if(isWinner == false){
-            checkForTie();
+            activePlayer = player2.getName();
+            activeMarker = player2.getMarker();
         }
     }
 
     function switchPlayer(){
-        activeMarker = activeMarker == playerMark ? computerMark : playerMark;
+        activePlayer = activePlayer == player1.getName() ? player2.getName() : player1.getName();
+        activeMarker = activeMarker == player1.getMarker() ? player2.getMarker() : player1.getMarker();
+    }
+
+    function checkForEmptySpaces(positionValue){
+        return positionValue !== '';
     }
 
     function checkForWinner(currentPlayerMove){
@@ -172,7 +173,7 @@ const GameFlow = (function(){
         filteredPossibleWins.forEach((array) => {
             if(array.every(checkForMarker)){
                 isWinner = true;
-                console.log(`${activeMarker} WINS!`);
+                console.log(`${activePlayer} WINS!`);
                 GameBoard.resetBoard();
                 console.log(board);
             }
@@ -187,13 +188,66 @@ const GameFlow = (function(){
         }
     }
 
+    function playGame(){
+
+        while(!(board.every(checkForEmptySpaces)) && isWinner == false){
+            let currentMove;
+            switchPlayer();
+
+            if(activePlayer == player1.getName()){
+                currentMove = player1.getPlayerMove();
+                GameBoard.addMark(player1.getMarker(), currentMove);
+            }else{
+                if(player2.hasOwnProperty('getComputerMove')){
+                    currentMove = player2.getComputerMove();
+                    GameBoard.addMark(player2.getMarker(), currentMove);
+                }else{
+                    currentMove = player2.getPlayerMove();
+                    GameBoard.addMark(player2.getMarker(), currentMove);
+                }
+            }
+            checkForWinner(currentMove);
+        }
+        
+        if(isWinner == false){
+            checkForTie();
+        }
+    }
+
     function startGame(){
-        assignPlayerMarker();
+        chooseOpponent();
+        assignFirstMarker();
         playXFirst();
         playGame();
     }
 
-    startGame();
+   // startGame();
+})();
+
+const DisplayGame = (function(){
+    const board = GameBoard.getBoard();
+    const body = document.querySelector('body');
+
+    function updateUi(){
+        const boardContainer = document.createElement('div');
+        boardContainer.classList.add('board-container');
+
+        boardContainer.style.display = 'grid';
+        boardContainer.style.gridTemplate = 'repeat(3, 50px) / repeat(3, 50px)';
+
+        body.appendChild(boardContainer);
+
+        board.forEach((element) => {
+            const div = document.createElement('div');
+            div.classList.add('single-space');
+            div.classList.add(`index-${board.indexOf(element)}`);
+            div.textContent = element;
+            boardContainer.appendChild(div);
+        });
+    }
+    
+    updateUi();
+
 })();
 
 
